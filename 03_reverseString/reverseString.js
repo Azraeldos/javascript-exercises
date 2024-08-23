82dd6bb5b2ca0d06ad78250c06882968aac3effd
Azraeldos <144492634+Azraeldos@users.noreply.github.com>
2024-08-23 16:59:44 -0700

    Competed exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    
+    const strRev =  str.split('').reverse().join('');
+    return strRev;  
 };
 
 // Do not edit below this line
