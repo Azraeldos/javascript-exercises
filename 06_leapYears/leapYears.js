00d9fbee65526e8e82a3671a88946e8dc1a51d86
Azraeldos <144492634+Azraeldos@users.noreply.github.com>
2024-08-29 09:25:37 -0700

    Completed exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+
+if (
+    year % 100 === 0 ? year % 400 === 0 : year % 4 === 0
+){
+    return true;
+}
+   
+else { return false; }
 
 };
-
+// run node on the file and console log the results to figure out questions
 // Do not edit below this line
 module.exports = leapYears;
