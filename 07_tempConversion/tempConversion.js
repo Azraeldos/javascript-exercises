b93184b9a855c5ab4a0497fd96c052c02d2c1efa
Azraeldos <144492634+Azraeldos@users.noreply.github.com>
2024-08-30 13:13:27 -0700

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  celsius = (num - 32)/1.8
+  celsius = Number(celsius.toFixed(1))
+  return celsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahrenheit = (temp * 1.8) + 32
+  fahrenheit = Number(fahrenheit.toFixed(1));
+  return fahrenheit;
 };
 
 // Do not edit below this line
